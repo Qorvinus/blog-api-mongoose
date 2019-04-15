948c51e304913634ac40cf542eb17bca7c942573
Xiong Moua <xiongmoua@hotmail.com>
2019-04-14 22:02:31 -0500

    lots of new base code

---
+++ models.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const mongoose = require("mongoose");
+
+const blogSchema = mongoose.Schema({
+  title: {type: String, require: true},
+  content: {type: String, require: true},
+  author: {
+      {firstName: {type: String, require: true}},
+      {lastName: {type: String, require: true}}
+      }
+});
+
+blogSchema.virtual("fullName").get(function() {
+  return `${this.firstName} ${this.lastName}`.trim();
+});
+
+blogSchema.methods.serialize = function() {
+  return {
+    id: this._id,
+    title: this.title,
+    content: this.content,
+    author: this.fullName,
+  };
+};
+
+const Blogposts = mongoose.model("Blogposts", blogSchema);
+
+module.exports = { Blogposts };
